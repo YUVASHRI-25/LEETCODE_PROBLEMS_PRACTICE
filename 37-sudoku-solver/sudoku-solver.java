8ab6d2e8903f400a9a2f4656c60ec844bdb2e24a
YUVASHRI <yuvashri195@gmail.com>
2026-03-31 12:05:39 +0530

    Time: 381 ms (20.18%) | Memory: 41.1 MB (99.93%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                // Find empty cell
+                if (board[i][j] == '.') {
+
+                    // Try numbers 1 to 9
+                    for (char c = '1'; c <= '9'; c++) {
+
+                        if (isValid(board, i, j, c)) {
+
+                            board[i][j] = c; // place number
+
+                            // Recurse
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Backtrack
+                            board[i][j] = '.';
+                        }
+                    }
+
+                    return false; // no valid number found
+                }
+            }
+        }
+
+        return true; // solved
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char c) {
+
+        for (int i = 0; i < 9; i++) {
+
+            // Check row
+            if (board[row][i] == c) return false;
+
+            // Check column
+            if (board[i][col] == c) return false;
+
+            // Check 3x3 box
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+
+            if (board[boxRow][boxCol] == c) return false;
+        }
+
+        return true;
+    }
+}
