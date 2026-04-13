4e5486a1a8f9ea414071de4bf5a388a9ad004f6a
YUVASHRI <yuvashri195@gmail.com>
2026-04-13 19:10:31 +0530

    Time: 774 ms (5.47%) | Memory: 48.7 MB (5.36%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        String ans = "";
+
+        for (int i = 0; i < num.length(); i++) {
+            char ch = num.charAt(i);
+
+          
+            if ((ch - '0') % 2 == 1) {
+                ans = num.substring(0, i + 1);
+            }
+        }
+
+        return ans;
+    }
+}
