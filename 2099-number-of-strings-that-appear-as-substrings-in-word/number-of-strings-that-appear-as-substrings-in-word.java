dccd921adc002e3ac021ad72d6c3b754f7ea1678
YUVASHRI <yuvashri195@gmail.com>
2026-06-29 10:14:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (56.03%) - LeetSync

---
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int  c=0;
+        for(String s:patterns)
+           if(word.indexOf(s)!=-1) c++;
+        return c;
+    }
+}
