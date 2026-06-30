b7be1cd9516b4008e2bc2c487b2cac07a463192e
YUVASHRI <yuvashri195@gmail.com>
2026-06-30 10:11:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (16.98%) - LeetSync

--- 292-nim-game/nim-game.java
+++ 292-nim-game/nim-game.java
@@ -1,13 +1,9 @@
 class Solution {
-    public boolean canWinNim(int n) 
-    {
+    public boolean canWinNim(int n) {
         if(n%4==0)
         {
             return false;
         }
-        else
-        {
-            return true;
-        }
+        else return true;
     }
 }
