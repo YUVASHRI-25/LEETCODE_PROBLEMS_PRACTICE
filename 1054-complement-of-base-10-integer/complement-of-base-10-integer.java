5137ed58ae86ca9056db9e0f94e1baad3d409ee1
YUVASHRI <yuvashri195@gmail.com>
2026-07-11 20:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (94.05%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n==0)
+        {
+            return 1;
+        }
+        int a=1;
+        while(a<n)
+        {
+            a=(a<<1)|1;
+        }
+        return n^a;
+    }
+}
