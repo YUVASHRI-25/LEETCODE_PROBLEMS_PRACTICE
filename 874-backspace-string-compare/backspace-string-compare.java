3529cd6cdcb496d4cc1ec9f763c2302e37856487
YUVASHRI <yuvashri195@gmail.com>
2026-07-24 11:29:59 +0530

    Time: 3 ms (20.70%) | Memory: 43.3 MB (20.29%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+
+        Stack<Character> stack1 = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+
+        // Process first string
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch != '#') {
+                stack1.push(ch);
+            } else if (!stack1.isEmpty()) {
+                stack1.pop();
+            }
+        }
+
+        // Process second string
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (ch != '#') {
+                stack2.push(ch);
+            } else if (!stack2.isEmpty()) {
+                stack2.pop();
+            }
+        }
+
+        return stack1.equals(stack2);
+    }
+}
