1a1f8caeff69ed86977f511368958a7c33ce930b
YUVASHRI <yuvashri195@gmail.com>
2026-08-29 21:05:09 +0530

    Time: 6 ms (73.94%) | Memory: 47 MB (42.16%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (arr[left] != arr[right]) {
+
+                if (arr[left] < arr[right]) {
+                    arr[right] = arr[left];
+                } else {
+                    arr[left] = arr[right];
+                }
+            }
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+}
